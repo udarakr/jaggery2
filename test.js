b465fa357f6fb9d946628144c9984fc32691ddf6
udarakr <udarakr@gmail.com>
2016-10-14 14:55:29 +0530

    adding sample script to test urimatcher

--- test.js
+++ test.js
@@ -1,3 +1,12 @@
-var Log = Java.type('org.jaggeryjs2.Log');
+/*var Log = Java.type('org.jaggeryjs2.Log');
 var log = new Log("test.js");
-log.info("test info log..");
+log.info("test info log..");*/
+
+var URIMatcher = Java.type('org.jaggeryjs2.URIMatcher');
+var uriMatcher = new URIMatcher('/sample/test.jag');
+
+if(uriMatcher.match('/{dir0}/{page}')) {
+    //If pattern matches, elements can be accessed from their keys
+    print("dir0 element is : " + uriMatcher.elements().dir0)
+}
+
