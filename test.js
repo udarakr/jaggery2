2f5752bc650e0b060c5ee377b66eb5a580e81cde
udarakr <udarakr@gmail.com>
2016-10-18 13:31:52 +0530

    adding xhr test script

--- test.js
+++ test.js
@@ -1,12 +1,26 @@
+//Log test snippet
 /*var Log = Java.type('org.jaggeryjs2.Log');
 var log = new Log("test.js");
 log.info("test info log..");*/
 
-var URIMatcher = Java.type('org.jaggeryjs2.URIMatcher');
+//URIMatcher test snippet
+/*var URIMatcher = Java.type('org.jaggeryjs2.URIMatcher');
 var uriMatcher = new URIMatcher('/sample/test.jag');
 
 if(uriMatcher.match('/{dir0}/{page}')) {
     //If pattern matches, elements can be accessed from their keys
     print("dir0 element is : " + uriMatcher.elements().dir0)
-}
+}*/
+
+//XHR test snippet
+ var XMLHttpRequest = Java.type('org.jaggeryjs2.xhr.XMLHttpRequest');
+ var url = 'http://jaggeryjs.org/documentation.jag';
+ var xhr = new XMLHttpRequest();
+ xhr.open("GET", url);
+ xhr.setRequestHeader("user" , "udarar");
+xhr.send();
+
+ print("ReadyState : "+xhr.readyState());
+ print("<br>ResponseStatus : "+xhr.status());
+ print("<br>ResponseText : "+xhr.responseText());
 
